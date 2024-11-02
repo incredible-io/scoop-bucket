4ebeb40894c1736147112675e7b5cf4f5b0512ac
Cid <121961562+incredible-io@users.noreply.github.com>
2024-11-02 10:33:20 +0100

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "incredible-io/scoop-bucket:master"
+    [String]$upstream = "incredible-io/scoop-bucket:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
